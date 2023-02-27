056d8dd4caa2e1c19a7c8c9a31976696bc9426db
MARFY <118571529+M4RFY@users.noreply.github.com>
2023-02-27 15:59:37 +0600

    Add files via upload

---
+++ mrf-hookers/__resource.lua
@@ -0,0 +1,34 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'Marfy, GLDNRMZ'
+version '1.2.0'
+description 'sawu_hookers converted for QBCore Framework'
+
+server_scripts {
+	'server/main.lua',
+	'config.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+	'config.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+	'ui/index.html',
+	'ui/libraries/axios.min.js',
+	'ui/libraries/vue.min.js',
+	'ui/libraries/vuetify.css',
+	'ui/libraries/vuetify.js',
+	'ui/script.js',
+	'ui/style.css',
+	-- images
+	'ui/img/Vennesa.png',
+	'ui/img/Cathrine.png',
+	'ui/img/Tatiana.png',
+	'ui/img/Bootylicious.png',
+	'ui/img/blowjob.png',
+	'ui/img/sex.png',
+}
